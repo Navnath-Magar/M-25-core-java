28bf8748baa8c411e4e12d57a8084a1f1fe021fc
Navnath <Navnath@DESKTOP-6V8V826>
2022-09-27 23:53:53 +0530

    first commit

--- day-2 introduction to java-variables,datat ypes and input/src/org/tnsindia/userinput/demoOninteger.java
+++ day-2 introduction to java-variables,datat ypes and input/src/org/tnsindia/userinput/demoOninteger.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class demoOninteger {
 
 	public static void main(String[] args) {
-		// for user input
+		// for user input 
 		Scanner s=new Scanner(System.in);
 		System.out.println("Enter the value of x:");
 		int x=s.nextInt();
