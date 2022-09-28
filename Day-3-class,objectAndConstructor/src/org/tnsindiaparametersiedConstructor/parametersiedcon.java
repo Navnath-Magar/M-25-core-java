042d5b59240b191f6e0a0110491d8969cf4dc246
Navnath <Navnath@DESKTOP-6V8V826>
2022-09-28 22:56:08 +0530

    second commit

---
+++ Day-3-class,objectAndConstructor/src/org/tnsindiaparametersiedConstructor/parametersiedcon.java
@@ -0,0 +1,10 @@
+package org.tnsindiaparametersiedConstructor;
+
+public class parametersiedcon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//parametersied
+	}
+
+}
